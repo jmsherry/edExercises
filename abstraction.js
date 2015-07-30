b3cc6f5d7bec5752568f78910a15dd2ef9aa690a
James Sherry <james.sherry@novafabrik.com>
2015-07-30 21:23:26 +0100

    uncommented

--- abstraction.js
+++ abstraction.js
@@ -17,21 +17,21 @@ var mandy = new Secretary();
 
 console.log(mandy.greet());
 
-// // But what if we need to greet Egita?
-//
-// Secretary.prototype.greetEgita = function(){
-//   return "Hello Egita"
-// };
-//
-// console.log(mandy.greetEgita());
-//
-// // BUT SERIOUSLY?!! This could go on and on... We can't write a new function for every possibility, so how about this:
-//
-// Secretary.prototype.greetGuest = function(guestName){
-//   return "Hello " + guestName; //When you 'return' something in a function it's what the function spits out.
-// };
-//
-// console.log(mandy.greetGuest('Julie'));
-// console.log(mandy.greetGuest('Ella'));
-//
-// // We abstracted the problem
+// But what if we need to greet Egita?
+
+Secretary.prototype.greetEgita = function(){
+  return "Hello Egita"
+};
+
+console.log(mandy.greetEgita());
+
+// BUT SERIOUSLY?!! This could go on and on... We can't write a new function for every possibility, so how about this:
+
+Secretary.prototype.greetGuest = function(guestName){
+  return "Hello " + guestName; //When you 'return' something in a function it's what the function spits out.
+};
+
+console.log(mandy.greetGuest('Julie'));
+console.log(mandy.greetGuest('Ella'));
+
+// We abstracted the problem
