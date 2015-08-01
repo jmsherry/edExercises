8171c80ac9cb6cd7a98f307d4a50c106963d0f32
James Sherry <james.sherry@novafabrik.com>
2015-08-01 22:28:41 +0100

    my solution for yesterday's problem

--- controlStructures.js
+++ controlStructures.js
@@ -8,3 +8,24 @@ function EdMultiply(str, a, b){
 
   return str + multiply(a,b)
 }
+
+
+$('body').html('').append('<button>CLick me</button><div id="test"></div>').children().click(function(e){
+  console.log('click');
+var $this = $(this), $div = $('#test'), txt = $div.text();
+
+console.log($this, txt);
+
+  switch(txt){
+   case '':
+   $div.text(names[0]);
+   break;
+   case names[0]:
+   $this.text(names[1]);
+   break;
+   case names[1]:
+   $this.text(names[0]);
+   break;
+  }
+
+});
